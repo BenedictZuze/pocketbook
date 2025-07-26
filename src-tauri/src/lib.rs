72d3ef803621de2523af89d35e40b7d2d0a35d0e
Benedict Zuze <96886292+BenedictZuze@users.noreply.github.com>
2025-07-26 11:01:26 +0000

    Jul 26, 2025, 1:01 PM

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,28 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
+use tauri_plugin_shell::process::CommandEvent;
+use tauri_plugin_shell::ShellExt;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let app_handle = app.app_handle().clone();
+            tauri::async_runtime::spawn(async move {
+                let sidecar = app_handle.shell().sidecar("pocketbase").unwrap();
+                let (mut rx, _child) = sidecar.spawn().expect("Failed to start PocketBase");
+
+                while let Some(event) = rx.recv().await {
+                    if let CommandEvent::Stdout(line) = event {
+                        println!("{}", String::from_utf8_lossy(&line));
+                    }
+                }
+                // No need to wait or kill here
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
