eaaeee823f6504e27034f7d2a922b6c755781063
Benedict Zuze <96886292+BenedictZuze@users.noreply.github.com>
2025-08-12 11:16:26 +0000

    Aug 12, 2025, 1:16 PM

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,45 @@ import { useEffect } from "react";
 import { Route, Router, Switch } from "wouter";
 import { Home } from "./pages/Home";
 import { Layout } from "./components/Layout";
-import { Provider, useAtom } from "jotai";
+import { Provider, useAtom, useAtomValue } from "jotai";
 import { NewProject } from "./pages/NewProject";
 import { ProjectDetails } from "./pages/ProjectDetails";
-import { projectsAtom } from "./store";
+import { dbAtom, pbAtom, projectsAtom } from "./store";
 import { listen } from "@tauri-apps/api/event";
+import { useSingleEffect } from "react-haiku";
+import { PocketBaseProject } from "./types";
 
 function App() {
+  const pb = useAtomValue(pbAtom);
+  const [, setDb] = useAtom(dbAtom);
   const [projects, setProjects] = useAtom(projectsAtom);
+  useSingleEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const projects = await pb
+          .collection("projects")
+          .getFullList<PocketBaseProject>();
+        setProjects(projects);
+      } catch (error) {
+        console.error("Failed to fetch projects:", error);
+      }
+    };
+    const authUser = async () => {
+      try {
+        const user = await pb
+          .collection("_superusers")
+          .authWithPassword(
+            import.meta.env.VITE_MASTER_EMAIL,
+            import.meta.env.VITE_MASTER_PASSWORD
+          );
+        setDb(user);
+      } catch (error) {
+        console.error("Failed to auth user:", error);
+      }
+    };
+    authUser();
+    fetchProjects();
+  });
 
   useEffect(() => {
     const unlistenPromise = listen<{ pid: string; isHealthy: boolean }>(
