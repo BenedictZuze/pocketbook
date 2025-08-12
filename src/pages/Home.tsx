9bb8dbe6ca9fb1cdc9f03da37b6f913f19c637dd
Benedict Zuze <96886292+BenedictZuze@users.noreply.github.com>
2025-08-12 11:18:12 +0000

    Aug 12, 2025, 1:18 PM

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,41 @@
 import React from "react";
 import { ProjectList } from "../components/ProjectList";
-import { useAtom, useAtomValue } from "jotai";
-import { dbAtom, pbAtom, projectsAtom } from "../store";
-import { useSingleEffect } from "react-haiku";
-import { PocketBaseProject } from "../types";
+// import { useAtom, useAtomValue } from "jotai";
+// import { dbAtom, pbAtom, projectsAtom } from "../store";
+// import { useSingleEffect } from "react-haiku";
+// import { PocketBaseProject } from "../types";
 
 export const Home: React.FC = () => {
-  const pb = useAtomValue(pbAtom);
-  const [, setDb] = useAtom(dbAtom);
-  const [, setProjects] = useAtom(projectsAtom);
-  useSingleEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const projects = await pb
-          .collection("projects")
-          .getFullList<PocketBaseProject>();
-        setProjects(projects);
-      } catch (error) {
-        console.error("Failed to fetch projects:", error);
-      }
-    };
-    const authUser = async () => {
-      try {
-        const user = await pb
-          .collection("_superusers")
-          .authWithPassword(
-            import.meta.env.VITE_MASTER_EMAIL,
-            import.meta.env.VITE_MASTER_PASSWORD
-          );
-        setDb(user);
-      } catch (error) {
-        console.error("Failed to auth user:", error);
-      }
-    };
-    authUser();
-    fetchProjects();
-  });
+  // const pb = useAtomValue(pbAtom);
+  // const [, setDb] = useAtom(dbAtom);
+  // const [, setProjects] = useAtom(projectsAtom);
+  // useSingleEffect(() => {
+  //   const fetchProjects = async () => {
+  //     try {
+  //       const projects = await pb
+  //         .collection("projects")
+  //         .getFullList<PocketBaseProject>();
+  //       setProjects(projects);
+  //     } catch (error) {
+  //       console.error("Failed to fetch projects:", error);
+  //     }
+  //   };
+  //   const authUser = async () => {
+  //     try {
+  //       const user = await pb
+  //         .collection("_superusers")
+  //         .authWithPassword(
+  //           import.meta.env.VITE_MASTER_EMAIL,
+  //           import.meta.env.VITE_MASTER_PASSWORD
+  //         );
+  //       setDb(user);
+  //     } catch (error) {
+  //       console.error("Failed to auth user:", error);
+  //     }
+  //   };
+  //   authUser();
+  //   fetchProjects();
+  // });
   return (
     <div>
       <div className="mb-8">
